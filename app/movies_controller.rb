a11de0f63eba522918cb715a7f49374292ce28c8
Joann Pan <joannlpan@gmail.com>
2019-01-24 04:15:58 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -70,7 +70,7 @@ def can_update_multiple_items_at_once
       film.update(title: "A Movie")
     end
  end
-end
+
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
