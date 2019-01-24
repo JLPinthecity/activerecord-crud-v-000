259fa54fe13e05f9737e8a0050f370f9570d7402
Joann Pan <joannlpan@gmail.com>
2019-01-24 04:15:36 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -63,11 +63,13 @@ def can_update_using_update_method
 end
 
 def can_update_multiple_items_at_once
-  # Change title of all movies to "A Movie"
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+    Movie.all.each do |film|
+      film.update(title: "A Movie")
+    end
+  end
 end
 
 def can_destroy_a_single_item
